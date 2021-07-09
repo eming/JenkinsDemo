66fad40fab04b64a809c6ba3165475b8a3ddb42e
emin <eminguliyev1987@gmail.com>
2021-07-09 14:51:56 +0200

    add thresholds

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 import { check, fail } from 'k6';
 export let options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'],   // http errors should be less than 1% 
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
   vus: 10,
   duration: '30s',
 };
 export default function () {
   let res = http.get('https://eminguliyev1987.com/');
   sleep(1);
-    check(res, {
+  check(res, {
     'response code was 200': (res) => res.status == 200,
     'body size was 1234 bytes': (res) => res.body.length == 1234,
   });
